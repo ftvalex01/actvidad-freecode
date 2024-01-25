7cb0b4ad0d4d3dac85a59c4b7b421851be145ca0
ftvalex01 <ftvalex@hotmail.com>
2024-01-25 17:20:47 +0000

    ejercicio 1 definitivo

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+app.use(cors({optionsSuccessStatus: 200}));
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
 function getTimestampAndUTC(dateString) {
   let date;
 
   if (!dateString) {
-      date = new Date();
-  } else if (!isNaN(parseInt(dateString))) {
-      date = new Date(parseInt(dateString));
+    date = new Date();
   } else {
+    // Comprobamos si dateString es un número (timestamp)
+    const timestamp = parseInt(dateString);
+    if (!isNaN(timestamp)) {
+      date = new Date(timestamp);
+    } else {
+      // Si no es un número, tratamos de parsearlo como una fecha
       date = new Date(dateString);
+    }
   }
 
   if (isNaN(date.getTime())) {
-      return { error: "Invalid Date" };
+    return { error: "Invalid Date" };
   }
 
   return { unix: date.getTime(), utc: date.toUTCString() };
@@ -46,11 +41,6 @@ app.get('/api/:date?', (req, res) => {
   res.json(result);
 });
 
-app.get('/api/1451001600000', (req, res) => {
-  res.json({ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" });
-});
-
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
